f2457374e91667141de50d124ae9f9461cb7a6bc
Ibrahim Abdelhamed <i_abdulhamid@outlook.com>
2020-10-18 13:50:09 +0200

    using Promises-02

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -10,14 +10,26 @@ export class DishService {
   constructor() { }
 
   getDishes(): Promise<Dish[]> {   
-    return Promise.resolve(DISHES);
+    //return Promise.resolve(DISHES);
+    return new Promise(resolve=> {
+      // Simulate server latency with 3 second delay
+        setTimeout(() => resolve(DISHES), 3000);
+    });
   }
   
   getDish(id: string): Promise<Dish> {
-    return Promise.resolve( DISHES.filter((dish) => (dish.id === id))[0] );
+    //return Promise.resolve( DISHES.filter((dish) => (dish.id === id))[0] );
+    return new Promise(resolve=> {
+      // Simulate server latency with 3 second delay
+        setTimeout(() => resolve( DISHES.filter((dish) => (dish.id === id))[0] ), 3000);
+    });
   }
 
   getFeaturedDish(): Promise<Dish> {
-    return Promise.resolve( DISHES.filter((dish) => dish.featured)[0] );
+    //return Promise.resolve( DISHES.filter((dish) => dish.featured)[0] );
+    return new Promise(resolve=> {
+      // Simulate server latency with 3 second delay
+        setTimeout(() => resolve( DISHES.filter((dish) => dish.featured)[0] ), 3000);
+    });
   }
 }
--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -10,15 +10,27 @@ export class LeaderService {
   constructor() { }
 
   getLeaders(): Promise<Leader[]> {
-    return Promise.resolve(LEADERS);
+    //return Promise.resolve(LEADERS);
+    return new Promise(resolve=> {
+      // Simulate server latency with 3 second delay
+        setTimeout(() => resolve(LEADERS), 1000);
+    });
   }
   
   getLeader(id: string): Promise<Leader> {
-    return Promise.resolve( LEADERS.filter((leader) => (leader.id === id))[0] );
+    //return Promise.resolve( LEADERS.filter((leader) => (leader.id === id))[0] );
+    return new Promise(resolve=> {
+      // Simulate server latency with 3 second delay
+        setTimeout(() => resolve( LEADERS.filter((leader) => (leader.id === id))[0] ), 1000);
+    });
   }
 
   getFeaturedLeader(): Promise<Leader> {
-    return Promise.resolve( LEADERS.filter((leader) => leader.featured)[0] );
+    //return Promise.resolve( LEADERS.filter((leader) => leader.featured)[0] );
+    return new Promise(resolve=> {
+      // Simulate server latency with 3 second delay
+        setTimeout(() => resolve( LEADERS.filter((leader) => leader.featured)[0] ), 1000);
+    });
   }
   
 }
